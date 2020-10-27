ba9c4d5dc79e0d90686e6c4d91f87966a05feb2b
BonJamon <56731427+BonJamon@users.noreply.github.com>
2020-10-27 14:49:03 +0100

    Finished: Knn + SVM

--- Other_Classifiers.py
+++ Other_Classifiers.py
@@ -1,10 +1,11 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn.pipeline import make_pipeline
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.decomposition import PCA
-from sklearn import svm
+from sklearn.svm import SVC
 
 
 
@@ -21,35 +22,62 @@ X_val, X_test, Y_val, Y_test = model_selection.train_test_split(X_val_test,Y_val
 
 #Preprocessing
 explained_variance = []
-dimensions = range(2,8)
+dimensions = range(2,20)
 for i in dimensions:
     pca = PCA(n_components=i)
     pca.fit(X_train)
 
     explained = 0
     for j in range(1,i):
-        explained += pca.explained_variance[j]
-
+        explained += pca.explained_variance_[j]
+        
     explained_variance.append(explained)
 
 #Search a good dimension
 plt.figure(figsize=(10,10))
-plt.ylim(0,1)
+plt.ylim(0,2000000)
 plt.plot(dimensions, explained_variance, label="explained_variance")
 plt.title("Explained Variance")
 plt.xlabel("dimension")
 plt.legend()
 
-pca = ...
+plt.show()
+
+pca = PCA(n_components = 12)
+
 #models
+degrees = range(1,8)
+
+#k-nn classifier
+acc_train = []
+acc_val = []
+for k in degrees:
+    knn = make_pipeline(pca, KNeighborsClassifier(n_neighbors = k))
+    knn.fit(X_train, Y_train)
+
+    acc_train.append(knn.score(X_train,Y_train))
+    acc_val.append(knn.score(X_val,Y_val))
+
+plt.figure(figsize=(10,10))
+plt.ylim(0,1)
+plt.plot(degrees, acc_train, label="acc_train")
+plt.plot(degrees, acc_val, label="acc_val")
+plt.title("Accuracies")
+plt.xlabel("degree")
+plt.legend()
+
+plt.show()
+
+#best: k = 3
+
+
 #Support Vector Classifier
 acc_train = []
 acc_val = []
 
-degrees = range(1,8)
 for k in degrees:
-    svm = make_pipeline(pca, svm.SVC(kernel=poly, degree=k))
-    svm.fit(X_train)
+    svm = make_pipeline(pca, SVC(kernel="poly", degree=k))
+    svm.fit(X_train, Y_train)
 
     acc_train.append(svm.score(X_train, Y_train))
     acc_val.append(svm.score(X_val,Y_val))
@@ -62,25 +90,8 @@ plt.title("Accuracies")
 plt.xlabel("degree")
 plt.legend()
 
-
-#K-NN Classifier
-acc_train = []
-acc_val = []
-
-for k in degrees:
-    knn = make_pipeline(pca, KNeighborsClassifier(n_neighbors = k))
-    knn.fit(X_train)
-
-    acc_train.append(knn.score(X_train,Y_train))
-    acc_val.append(score(X_val,Y_val))
-
-plt.figure(figsize=(10,10))
-plt.ylim(0,1)
-plt.plot(degrees, acc_train, label="acc_train")
-plt.plot(degrees, acc_val, label="acc_val")
-plt.title("Accuracies")
-plt.xlabel("degree")
-plt.legend()
+plt.show()
+#best is k = 3
 
 
 #Testing
